5768a15b740994596348822305f67d0147a97f5f
hetsaraiya <hetsaraiya06@gmail.com>
2024-06-11 13:42:06 +0530

    chore: Update CSRF_TRUSTED_ORIGINS in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ["https://carconnectbackend-production.up.railway.app"]
 
 # Application definition
 
